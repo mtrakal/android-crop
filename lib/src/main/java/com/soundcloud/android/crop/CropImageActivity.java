cf993687bb2a58d840d7175aa80ec9c198e6f0de
Sergii Nezdolii <sergii.nezdolii@gmail.com>
2015-07-24 14:19:35 +0200

    Hotfix of Samsung devices issue based on https://github.com/jdamcd/android-crop/pull/146

--- lib/src/main/java/com/soundcloud/android/crop/CropImageActivity.java
+++ lib/src/main/java/com/soundcloud/android/crop/CropImageActivity.java
@@ -325,6 +325,14 @@ public class CropImageActivity extends MonitoredActivity {
                 RectF adjusted = new RectF();
                 matrix.mapRect(adjusted, new RectF(rect));
 
+                //if the cutting box are rectangle( outWidth != outHeight ),and the exifRotation is 90 or 270,
+                //the outWidth and outHeight should be interchanged
+                if (exifRotation==90||exifRotation==270){
+                    int temp=outWidth;
+                    outWidth=outHeight;
+                    outHeight=temp;
+                }
+
                 // Adjust to account for origin at 0,0
                 adjusted.offset(adjusted.left < 0 ? width : 0, adjusted.top < 0 ? height : 0);
                 rect = new Rect((int) adjusted.left, (int) adjusted.top, (int) adjusted.right, (int) adjusted.bottom);
@@ -335,6 +343,14 @@ public class CropImageActivity extends MonitoredActivity {
                 if (rect.width() > outWidth || rect.height() > outHeight) {
                     Matrix matrix = new Matrix();
                     matrix.postScale((float) outWidth / rect.width(), (float) outHeight / rect.height());
+
+                    //If the picture's exifRotation !=0 ,they should be rotated to 0 degrees
+                    matrix.postRotate(exifRotation);
+                    croppedImage = Bitmap.createBitmap(croppedImage, 0, 0, croppedImage.getWidth(), croppedImage.getHeight(), matrix, true);
+                } else {
+                    //if the picture need not to be scale, they also need to be rotate to 0 degrees
+                    Matrix matrix = new Matrix();
+                    matrix.postRotate(exifRotation);
                     croppedImage = Bitmap.createBitmap(croppedImage, 0, 0, croppedImage.getWidth(), croppedImage.getHeight(), matrix, true);
                 }
             } catch (IllegalArgumentException e) {
